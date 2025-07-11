4cae8d975fafacc756b8b68ee668087a9f4a620e
Zdravko Danev <zdanev@gmail.com>
2025-07-10 22:14:27 -0700

    added mailboxes list view

--- Views/MainWindow.cs
+++ Views/MainWindow.cs
@@ -35,5 +35,29 @@ public class MainWindow : Window
             new StatusItem(Key.F3, "~F3~ Load", null)
         });
         Application.Top.Add (statusBar);
+
+        var MailboxesListView = new ListView()
+        {
+            X = 0,
+            Y = 0,
+            Width = 25,
+            Height = Dim.Fill(),
+            // Border = new Border()
+            // {
+            //     Title = "Mailboxes",
+            //     BorderStyle = Terminal.Gui.BorderStyle.Single
+            // },
+        };
+        MailboxesListView.SetSource(new List<string> () { "Inbox", "Sent", "Drafts", "Trash" });
+        Add(MailboxesListView);
+
+        // var verticalLine = new LineView(Orientation.Vertical)
+        // {
+        //     X = Pos.Right(MailboxesListView),
+        //     Y = 0,
+        //     Height = Dim.Fill(),
+        //     // Width = 1
+        // };
+        // Add(verticalLine);
     }
 }
